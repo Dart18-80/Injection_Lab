dc2f472f53954bbaf9b71300fbaf1d15897411d2
Dart18-80 <diegoruiztrabanino13@gmail.com>
2023-01-21 23:17:51 -0600

    Agregar archivos de proyecto.

---
+++ Injection_Lab/Repositories/CustomerRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Injection_Lab.Repositories
+{
+    public class CustomerRepository : IRepository
+    {
+        private IDbConnection _connection;
+
+        public CustomerRepository(IDbConnection connectionDB) 
+        {
+            _connection = connectionDB;
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            if (_connection.GetType() == typeof(MysqlConnection))
+                Console.WriteLine("Obtengo los clientes de Mysql");
+            else if (_connection.GetType() == typeof(OracleConnection))
+                Console.WriteLine("Obtengo los clientes de Oracle");
+
+            return new List<Customer> 
+            {
+                new Customer() {Id = 1, Name = "Diego Ruiz", Email = "diegoruiz@gmail.com", Phone = "45123698"},
+                new Customer() {Id = 2, Name = "Luis Ruiz", Email = "luisruiz@gmail.com", Phone = "42169876"}
+            };
+        }
+    }
+}
